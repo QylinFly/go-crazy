06198085b2a05c5600860d1bce58cc52f8390e4f
Qylin <18612116114@163.com>
2018-05-22 20:31:57 +0800

    init docker

--- docker/build.app.sh
+++ docker/build.app.sh
@@ -2,6 +2,9 @@
 # ln -s $PWD/vendor/github.com/golang vendor/golang.org/x
 
 cd ..
+
 cp .env.yml ./docker
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./docker/server.exe .
+
+cd ./docker
 
